4acfc872cf3b0a0a36e7f2a126c195ad011030e3
khazmli <togrul.dj@gmail.com>
2024-05-12 17:33:23 +0300

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+let arr = [2, 5, 7];
+function edediOrta () {
+    let a = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        a +=arr[i]
+        sum = a / arr.length 
+    }
+    console.log(sum);
+}
+edediOrta();
+
+
